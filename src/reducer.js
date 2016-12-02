a7546f674bbc6825eebf0b5fcc7c02d3320fd283
Tony Kung <lps.tony@gmail.com>
2016-12-01 21:46:13 -0600

    refactor for redux:1

---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+import { VisibilityFilters } from "./action"
+
+const initialState = {
+  VisibilityFilters: VisibilityFilters.SHOW_ALL,
+  todos: []  
+}
+
+function todoApp(state = initialState, action){
+
+  return state;
+}
