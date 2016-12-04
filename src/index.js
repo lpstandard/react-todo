c811a77c36421e898b956ad1de6a7c075d910782
Tony Kung <lps.tony@gmail.com>
2016-12-03 21:00:26 -0600

    refactor part2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoApp from './components/App';
+import { createStore } from 'redux';
+import todoApp from './reducer';
+let store = createStore(todoApp);
 
 ReactDOM.render(
   <TodoApp />,
--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,49 @@
-import { VisibilityFilters } from "./action"
+import { VisibilityFilters, ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from "./action"
+import { combineReducers } from "redux"
+const { SHOW_ALL } = VisibilityFilters
 
 const initialState = {
-  VisibilityFilters: VisibilityFilters.SHOW_ALL,
+  visibilityFilters: VisibilityFilters.SHOW_ALL,
   todos: []  
 }
 
-function todoApp(state = initialState, action){
+function todos(state = [], action){
+  switch (action.type){
+    case ADD_TODO:
+      return [
+        ...state, 
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
 
-  return state;
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+          if(index === action.index){
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            })
+          }
+      return todo
+    })
+  default;
+    return state
+  }
 }
+
+const todoApp = combineReducers({
+  visibilityFilter, 
+  todos
+})
+
+function visibilityFilter(state = SHOW_ALL, action){
+  switch(action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter
+    default; 
+      return state
+  }
+}
+
+export default todoApp
